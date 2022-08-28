0e8b5ef892de16371b46c6a5705e98c2152640cd
MashWac <macharia.wachira@strathmore.edu>
2022-08-28 15:00:52 +0300

    saadasdas

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -42,8 +42,9 @@ class ProductController extends Controller
             if(file_exists($request->hasFile('prodimage'))){
             $path = Storage::disk('s3')->put('images',file_get_contents($file));
             $path = Storage::disk('s3')->url($path);
-            $product->product_image=$path;
+            
             }
+            $product->product_image=$path;
         }
         $product->product_name=$request->input('prodname');
         $product->product_description=$request->input('proddescr');
