9d2580168a0d5a5f73ef5e1614fcecd210eaeed5
MashWac <macharia.wachira@strathmore.edu>
2022-08-30 16:51:44 +0300

    dbshfasbghsabmj

--- public/frontend/js/downloadpdf.js
+++ public/frontend/js/downloadpdf.js
@@ -10,6 +10,6 @@ window.onload =function(){
             html2canvas:{scale:2},
             jsPDF:{unit: 'in',format:'letter',orientation:'portrait'}
         }
-        html2pdf().from().save();
+        html2pdf().from(invoice).save();
     })
 }
