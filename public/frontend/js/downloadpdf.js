9a0a6e964ef7af556ed2bdac447912b74478e9a7
MashWac <macharia.wachira@strathmore.edu>
2022-08-30 16:31:29 +0300

    fandsjfnafhn

--- public/frontend/js/downloadpdf.js
+++ public/frontend/js/downloadpdf.js
@@ -4,10 +4,7 @@ window.onload =function(){
         console.log(invoice);
         console.log(window);
         var opt={
-            filename:'siensafricainvoice.pdf',
-            image:{type:'jpeg',quality: 0.98},
-            html2canvas:{scale:2},
-            jsPDF:{unit: 'in',format:'letter',orientation:'portrait'}
+            filename:'siensafricainvoice.pdf'
         };
         html2pdf().from(invoice).set(opt).save();
     })
