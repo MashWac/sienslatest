5e0057c8c4306a705135b59131570b84ad29898c
MashWac <macharia.wachira@strathmore.edu>
2022-08-30 16:20:56 +0300

    tyuilk

--- resources/views/user/receipt.blade.php
+++ resources/views/user/receipt.blade.php
@@ -11,7 +11,7 @@
                         Date of Purchase: {{$data['order']->created_at}}
                     </div>
             </div>
-            <div class="card-body" id='invoice'>
+            <div class="card-body" >
                 <div class="row mb-4">
                     <div class="col-sm-6">
                         <h5 class="mb-3">From:</h5>
@@ -28,7 +28,7 @@
                         <div>Phone:{{$data['user']->telephone}}</div>
                     </div>
                 </div>
-                <div class="table-responsive-sm">
+                <div class="table-responsive-sm" id='invoice'>
                     <table class="table table-striped">
                         <thead>
                             <tr>
