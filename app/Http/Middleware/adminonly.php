d694423cc7d506c453cc9f178744b4964b8eb590
MashWac <macharia.wachira@strathmore.edu>
2022-09-06 21:03:30 +0300

    jnkskjfnafjfsf afasjfanfsk

--- app/Http/Middleware/adminonly.php
+++ app/Http/Middleware/adminonly.php
@@ -18,7 +18,7 @@ class adminonly
      */
     public function handle(Request $request, Closure $next)
     {
-        if (Auth::user()->role_as=='admin'){
+        if (Auth::user()?->role_as!=1){
             abort(Response::HTTP_FORBIDDEN);
         }
         return $next($request);
