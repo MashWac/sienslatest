1bef90c840eba86845e100ed7072b9e052ffe5c2
MashWac <macharia.wachira@strathmore.edu>
2022-09-07 20:47:12 +0300

    finally pagination all 0709

--- resources/views/admin/category/view.blade.php
+++ resources/views/admin/category/view.blade.php
@@ -57,5 +57,8 @@
                 </table>
           
         </div>
+        <div class="text-center d-flex justify-content-center">
+            {{ $products->links('pagination::bootstrap-4') }}
+             </div>
     </div>
 @endsection
