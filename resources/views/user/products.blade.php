bac872a21c263768cecb144a5daf62cb30da4c30
MashWac <macharia.wachira@strathmore.edu>
2022-09-14 21:28:58 +0300

    view details change

--- resources/views/user/products.blade.php
+++ resources/views/user/products.blade.php
@@ -33,7 +33,7 @@
                                     
                                     <div class="d-grid gap-2 d-md-block" style="position: absolute; bottom: 10%; height: 150px;">
                                         <h6 class="text-center pricetext"style="margin-top: 30%;">{{$item->unit_price}} KSH<h6>
-                                        <a href="{{url('viewproduct/'.$item->product_id)}}" class="btn btn-warning "id="btnpurch" style="color:white;"> View Product</a>
+                                        <a href="{{url('viewproduct/'.$item->product_id)}}" class="btn btn-warning "id="btnpurch" style="color:white;"> View Details</a>
                                         <a href="{{url('addtocart/'.$item->product_id)}}" class="btn btn-primary"> Add To Cart</a>
                                     </div>
                                 </div>
