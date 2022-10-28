74a6e07ab5611f2a6ebdee237a0d630cfd894385
MashWac <macharia.wachira@strathmore.edu>
2022-10-28 22:08:30 +0300

    somethingccra

--- resources/views/user/products.blade.php
+++ resources/views/user/products.blade.php
@@ -15,31 +15,25 @@
                     <li class="filteropts">
                     <!-- Example single danger button -->
                         <div class="btn-group">
-                        <button type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                        <button type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" list="categoricallist">
                             Action
                         </button>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            <li><a class="dropdown-item" href="#">Separated link</a></li>
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            <li><a class="dropdown-item" href="#">Separated link</a></li>
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            <li><a class="dropdown-item" href="#">Separated link</a></li>
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            <li><a class="dropdown-item" href="#">Separated link</a></li>
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            <li><a class="dropdown-item" href="#">Separated link</a></li>
-                        </ul>
+                        <datalist id="categoricallist">
+                            <option value="Something">
+                            <option value="Something">
+                            <option value="Something">
+                            <option value="Something">
+                            <option value="Something">
+                            <option value="Something">
+                            <option value="Something">
+                            <option value="Something">
+                            <option value="Something">
+                            <option value="Something">
+                            <option value="Something">
+                            <option value="Something">
+                            <option value="Something">
+                            <option value="Something">
+                        </datalist>
                         </div>
                     </li>
 
