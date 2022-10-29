ea2e7c392b470d772ed98910196f066175e9489c
MashWac <macharia.wachira@strathmore.edu>
2022-10-29 11:10:29 +0300

    fixingcards

--- resources/views/user/products.blade.php
+++ resources/views/user/products.blade.php
@@ -42,13 +42,13 @@
             <div class="prodsectionprodpage container-fluid">
                     <div class="row">
                     @foreach($data['products'] as $item)
-                            <div class="card productsprofile h-100 " style="width: 16rem; " >
+                            <div class="card productsprofile h-100 " style="width: 18rem; " >
                                 <img src="{{ $item['product_image']}}"  class="card-img-top" height="300px"alt="...">
-                                <div class="card-body details" style="position: relative; height: 150px; ">
+                                <div class="card-body details">
 
                                     <h5 class="card-title">{{$item->product_name}}</h5>
                                     
-                                    <div class="d-grid gap-2 d-md-block" style="position: absolute; bottom: 10%; height: 150px;">
+                                    <div class="">
                                         <h6 class="text-center pricetext"style="margin-top: 30%;">{{$item->unit_price}} KSH<h6>
                                         <a href="{{url('viewproduct/'.$item->product_id)}}" class="btn btn-warning "id="btnpurch" style="color:white;"> View Details</a>
                                         <a href="{{url('addtocart/'.$item->product_id)}}" class="btn btn-primary"> Add To Cart</a>
