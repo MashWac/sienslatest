a27659e21e1e492a00948bd7eae306ed5e8b0095
MashWac <macharia.wachira@strathmore.edu>
2022-10-29 11:13:44 +0300

    fixingcards

--- resources/views/user/products.blade.php
+++ resources/views/user/products.blade.php
@@ -49,9 +49,11 @@
                                     <h5 class="card-title">{{$item->product_name}}</h5>
                                     
                                     <div class="">
-                                        <h6 class="text-center pricetext"style="margin-top: 30%;">{{$item->unit_price}} KSH<h6>
-                                        <a href="{{url('viewproduct/'.$item->product_id)}}" class="btn btn-warning "id="btnpurch" style="color:white;"> View Details</a>
-                                        <a href="{{url('addtocart/'.$item->product_id)}}" class="btn btn-primary"> Add To Cart</a>
+                                        <h6 class="text-center pricetext">{{$item->unit_price}} KSH<h6>
+                                        <div class="prodbuttons">
+                                            <a href="{{url('viewproduct/'.$item->product_id)}}" class="btn btn-warning "id="btnpurch" style="color:white;"> View Details</a>
+                                            <a href="{{url('addtocart/'.$item->product_id)}}" class="btn btn-primary"> Add To Cart</a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
